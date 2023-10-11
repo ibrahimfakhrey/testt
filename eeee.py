e1ca39553717c901613962d1bdb3d45e58ba7407
ibrahimfakhry <Ibrahim fakhrey@github.com>
2023-10-11 13:06:34 +0300

    rtuy56

--- eeee.py
+++ eeee.py
@@ -1,7 +1,31 @@
-import random
-x= random.randint(1,6)
-y=random.randint(1,6)
-if x >y:
-    print(f"salma wins {x} {y}")
-else:
-    print(f" judy wins {y } and salma {x }")
+import cv2
+import os
+
+CASCADE = 'faces.xml'
+INPUT_FOLDER = 'input'
+OUTPUT_FOLDER = 'output'
+COLOR = (255, 255, 255)
+WIDTH = 4
+SCALE = 1.1
+NEIGHBORS = 4
+
+
+def has_face(image_path):
+    image = cv2.imread(image_path, 1)
+    face_cascade = cv2.CascadeClassifier(CASCADE)
+
+    faces = face_cascade.detectMultiScale(image, SCALE, NEIGHBORS)
+    print(faces)
+    if len(faces) != 0:
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x + w, y + h), COLOR, WIDTH)
+
+        return image
+
+
+images = os.listdir(INPUT_FOLDER)
+for image_path in images:
+    print(image_path)
+    face_image = has_face(f'{INPUT_FOLDER}/{image_path}')
+    if face_image is not None:
+        cv2.imwrite(f'{OUTPUT_FOLDER}/{image_path}', face_image)
--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import cv2
-image_name=input("write the image name")
-image = cv2.imread(image_name, 1)
-face_cascade = cv2.CascadeClassifier('faces.xml')
+from flask import Flask,render_template,request,redirect
+
+app=Flask(__name__)
+
+
+@app.route ("/")
+def start():
+    return "start"
+@app.route("/check",methods=["GET","POST"])
+def check():
+    if request.method=="POST":
+        file = request.files['image']
+        file.save('image.jpg')
+
+        image = cv2.imread("image.jpg", 1)
+        face_cascade = cv2.CascadeClassifier('faces.xml')
+        faces = face_cascade.detectMultiScale(image, 1.1, 4)
+        for (x, y, w, h) in faces:
+            print("this is mister ramadan ")
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 255), 4)
+        new_image=cv2.imwrite('human_faces.jpeg', image)
+        return redirect("/image")
+    return render_template("check.html")
+
+
+@app.route("/image")
+def image():
+    return render_template("image.html")
 
 
 
-def find_mistre_ramadan(image):
-  faces = face_cascade.detectMultiScale(image, 1.1, 4)
-  for (x, y, w, h) in faces:
-    if x ==806 and y==162 and w==185 and h==185:
-      print("this is mister ramadan ")
-      cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 255), 4)
-      return True
-    else:
-      return False
-
-
-if find_mistre_ramadan(image):
-  print("this is the secret")
-else:
-  print("i didnot find mister ramadan ")
-
-
-# cv2.imwrite('human_faces.jpeg', image)
-
-
+if __name__=="__main__":
+    app.run(debug=True)
